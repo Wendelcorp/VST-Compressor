2060f4f962425464e551f8523880b71ce1126124
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2015-09-21 09:37:22 -0700

    DSP Gain Attenuation
    Adjusted signal processing equations so that gain attenuation works

--- VSTCompressor/VSTCompressor.cpp
+++ VSTCompressor/VSTCompressor.cpp
@@ -18,8 +18,8 @@ VSTCompressor::VSTCompressor(audioMasterCallback audioMaster)
 	//fGain = 1.f;			// default to 0 dB
 	// Parameters
 	ratio = 10.0;
-	thresh = 0.5;
-	makeup = 4.0;
+	thresh = 0.05;
+	makeup = 1.6;
 	cntrl1 = 1.0;
 	cntrl2 = 1.0;
 	knee = 0.0;
@@ -47,27 +47,37 @@ void VSTCompressor::getProgramName(char* name)
 // Proccessing function
 void VSTCompressor::processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames)
 {
-	float* in1 = inputs[0];
-	float* in2 = inputs[1];
-	float* out1 = outputs[0];
-	float* out2 = outputs[1];
+	float* in1 = inputs[1];
+	float* in2 = inputs[0];
+	float* out1 = outputs[1];
+	float* out2 = outputs[0];
 
-	while (--sampleFrames >= 0)
+	for (int i = 0; i < sampleFrames; i++)
 	{
 		
 		// Set control voltages
-		if (*in1 > thresh){
-			cntrl1 = thresh * ((*in1 - thresh) / (ratio));
+		if (in1[i] > thresh){
+			cntrl1 = thresh + ((in1[i] - thresh)/ratio);		// New gain equals threshold plus difference between input and threshold divided by the ratio
 		}
+		else { cntrl1 = in1[i]; }
 
-		if (*in2 > thresh){
-			cntrl2 = thresh * ((*in2 - thresh) / (ratio));
+
+		if (in2[i] > thresh){
+			cntrl2 = thresh + ((in2[i] - thresh) / ratio);
 		}
+		else { cntrl2 = in2[i]; }
 
 
-		// output sound
-		(*out1++) = (*in1++) * (makeup);
-		(*out2++) = (*in2++) * (makeup);
+		// develop output mix
+		out1[i] = (-1) * cntrl1 * makeup;
+		out2[i] = (-1) * cntrl2 * makeup;
+		/*
+		// Increment inputs and outputs to next frame
+		*in1++;
+		*in2++;
+		*out1++;
+		*out2++;
+		*/
 	}
 }
 
